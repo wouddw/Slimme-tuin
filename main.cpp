0e5d20e074eada7e53d3c50beb4e237733772168
wouddw <156894547+wouddw@users.noreply.github.com>
2025-06-02 16:44:40 +0200

    Add files via upload
    main van temperatuur -en luchtvochtigheid sensor

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Sensor.h"
+
+Sensor sensor; //maak een object van de Sensor klasse
+
+void setup() {
+    Serial.begin(115200); //start communicatie tussen esp32 en computer
+    sensor.begin(); //initialiseer de sensor
+}
+
+void loop() {
+    float temperatuur = sensor.getTemperatuur(); //zet de waarde van 'getTemperatuur' om in een float in 'temperatuur'
+    float luchtvochtigheid = sensor.getLuchtvochtigheid(); //zet de waarde van 'getLuchtvochtigheid' om in een float in 'luchtvochtigheid'
+
+    delay(2000); // wacht 2 seconden tussen metingen
+}
