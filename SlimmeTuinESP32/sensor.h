ab625929ee30c0df17a27c32c7812e5fad198d8b
vjuniorG <vincent.goemaere@vtibrugge.be>
2025-06-03 16:11:42 +0200

    Woud De Waegenaere sensor files copy added to main branch so full code can work.

---
+++ SlimmeTuinESP32/sensor.h
@@ -0,0 +1,18 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <Adafruit_AHTX0.h>
+
+class Sensor {
+private:
+    Adafruit_AHTX0 aht; //een object van de klasse Adafruit_AHTX0 uit de Adafruit bibliotheek
+    sensors_event_t temp; //opslag van temperatuur
+    sensors_event_t humidity; //opslag van luchtvochtigheid
+public: 
+    void begin(); //initialiseer sensor
+    float getTemperatuur(); //temperatuur 'getten'
+    float getLuchtvochtigheid(); //luchtvochtigheid 'getten'
+    int getBodemvochtigheid(); //bodemvochtigheid 'getten'
+};
+
+#endif
