acbb2baac9c3efa2097d1f58b3fe8e77a8e135b8
wouddw <156894547+wouddw@users.noreply.github.com>
2025-06-03 15:44:39 +0200

    main code van beide sensoren
    de main code van zowel de temperatuur/luchtvochtigheid sensor, als de bodemvochtigheid sensor

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ void setup() {
 void loop() {
     float temperatuur = sensor.getTemperatuur(); //zet de waarde van 'getTemperatuur' om in een float in 'temperatuur'
     float luchtvochtigheid = sensor.getLuchtvochtigheid(); //zet de waarde van 'getLuchtvochtigheid' om in een float in 'luchtvochtigheid'
+    int bodemvochtigheid = sensor.getBodemvochtigheid(); //zet de waarde van 'getBodemvochtigheid' om in een int in 'bodemvochtigheid'
 
     delay(2000); // wacht 2 seconden tussen metingen
 }
